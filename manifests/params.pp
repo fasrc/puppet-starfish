49cab9d6e5de601a64c407d747c41bae7d97cc36
Luis Silva <luis_silva@harvard.edu>
2016-09-06 11:25:36 -0400

    removing commas

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,15 +3,15 @@
 # parameters used by the module
 #
 class starfish::params {
-  $version         = undef,
-  $repo_url        = undef,
-  $repo_name       = 'Starfish',
-  $proxy           = undef,
-  $descr           = 'Starfish Repository',
-  $gpgcheck        = 0,
-  $gpgkey          = false,
-  $enabled         = 1,
-  $super_repo_name = 'starfish-supervisor',
-  $super_descr     = 'supervisor 3 Repository',
+  $version         = undef
+  $repo_url        = undef
+  $repo_name       = 'Starfish'
+  $proxy           = undef
+  $descr           = 'Starfish Repository'
+  $gpgcheck        = 0
+  $gpgkey          = false
+  $enabled         = 1
+  $super_repo_name = 'starfish-supervisor'
+  $super_descr     = 'supervisor 3 Repository'
   $super_repo_url  = undef
 }
